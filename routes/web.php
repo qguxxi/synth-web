e4b140be8a8d6da0b97b4bf5d526da1f31e028c2
Duc Hoang Manh <jandyz.dev@gmail.com>
2025-01-03 20:44:44 +0700

    add vendor package

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', function () {
+    return "Hello world";
+});
+
+Route::get('/product',
+     [ProductController::class, 'index'
+]);
